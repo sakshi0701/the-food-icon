132f189b57d07510f21555c8063c73a6a965281d
Sakshi Dwivedi <sakshithedwivedi@gmail.com>
2021-03-07 18:48:42 +0530

    commit

--- src/components/notes/CreateArea.jsx
+++ src/components/notes/CreateArea.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FaPlus } from 'react-icons/fa';
 
 function CreateArea({ submitButton, onAdd }) {
